2f0a805fb1ec8e54ec98308bd22e75bfeb48c8e6
Angelo Rafael Peres Pinto <angelorpt2@gmail.com>
2019-12-06 16:22:05 -0400

    Tipos e Status de respostas

--- my_express/routes.js
+++ my_express/routes.js
@@ -23,4 +23,25 @@ router.post('/body', (req, res) => {
     res.json(req.body);
 });
 
+// Text
+router.get('/res/text', (req, res) => {
+    res.status(201).send('teste');
+});
+
+// Json
+router.get('/res/json', (req, res) => {
+    res.status(201).json({
+        message: 'cadastrado com sucesso',
+        curso: 'Curso Express',
+        unidade: 'Tipo de Resposta'
+    });
+});
+
+// Render View
+// router.get('/res', (req, res) => {
+//     res.render('index', {
+
+//     });
+// });
+
 module.exports = router;
