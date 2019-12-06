cb384848ffea1f6aff85145da1ec66b2b22ca3e9
Angelo Rafael Peres Pinto <angelorpt2@gmail.com>
2019-12-06 14:32:36 -0400

    Utilizando o módulo HTTP do node

--- my_express/app.js
+++ my_express/app.js
@@ -1,11 +1,15 @@
 var express = require('express');
-var app = express();
+var http    = require('http');
+var app     = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World - Express');
+    res.send('Hello World - Express 2');
 });
 
-app.listen(3000, () => {
-    console.log('Express started - funcionando');
-});
+// app.listen(3000, () => {
+//     console.log('Express started - funcionando');
+// });
 
+http.createServer(app).listen(3000, function() {
+    console.log('Express started - with HTTP');
+});
