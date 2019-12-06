37096edfaa90e990a1744708c8605f794a49a897
Angelo Rafael Peres Pinto <angelorpt2@gmail.com>
2019-12-06 15:43:49 -0400

    Utilizando rotas e express.router

--- my_express/app.js
+++ my_express/app.js
@@ -1,15 +1,23 @@
 var express = require('express');
-var http    = require('http');
+// var http    = require('http');
+var routes  = require('./routes');
 var app     = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World - Express 2');
 });
 
-// app.listen(3000, () => {
-//     console.log('Express started - funcionando');
-// });
-
-http.createServer(app).listen(3000, function() {
-    console.log('Express started - with HTTP');
+app.get('/world', (req, res) => {
+    res.send('World');
 });
+
+app.use('/hello', routes);
+
+
+app.listen(3000, () => {
+    console.log('Express started - funcionando');
+});
+
+// http.createServer(app).listen(3000, function() {
+//     console.log('Express started - with HTTP');
+// });
