4ef8863c3bc10ecd8f6d38dea62301d304d90b91
Angelo Rafael Peres Pinto <angelorpt2@gmail.com>
2019-12-06 16:37:17 -0400

    Servindo arquivos estáticos

--- my_express/app.js
+++ my_express/app.js
@@ -1,10 +1,10 @@
 var express    = require('express');
-// var http    = require('http');
+var path       = require('path');
+var http       = require('http');
 var bodyParser = require('body-parser');
 
 var routes  = require('./routes');
 var app     = express();
-
 
 app.get('/', (req, res) => {
     res.send('Hello World - Express 2');
@@ -18,6 +18,10 @@ app.get('/world', (req, res) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/hello', routes);
+
+app.use('/public',express.static(path.join(__dirname, 'public')));
+
+
 
 app.listen(3000, () => {
     console.log('Express started - funcionando');
